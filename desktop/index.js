2e6125e0e0a57a53f8400c2a41972f2a136ec793
kvarma@jazeeratechnologies.com <kvarma@jazeeratechnologies.com>
2019-12-06 12:22:45 +0530

    10.0.6

--- desktop/index.js
+++ desktop/index.js
@@ -15,7 +15,6 @@ const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
 const url = require('url');
-autoUpdater.logger.transports.file.level = 'info';
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
