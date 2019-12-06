c5c351f230d5d6266621803d6d1a1336f06980b0
kvarma@jazeeratechnologies.com <kvarma@jazeeratechnologies.com>
2019-12-06 10:48:14 +0530

    commit v6

--- desktop/index.js
+++ desktop/index.js
@@ -41,8 +41,8 @@ app.on('ready', () => {
     mainWindow = null;
   });
   const server = 'https://github.com/krupavarma/electron-app-desktop';
-  const feed = server + '/releases';
-  autoUpdater.setFeedURL(feed);
+  // const feed = server + '/releases';
+  autoUpdater.setFeedURL(server);
   setInterval(() => {
     autoUpdater.checkForUpdates();
   }, 60000);
